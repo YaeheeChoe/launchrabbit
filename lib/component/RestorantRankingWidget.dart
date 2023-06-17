ec390ac6af75354b0a1cabfe14b325795be34a53
Joshooa <klasrptmxm@gmail.com>
2023-06-17 15:27:20 +0900

    임시 방문기록 랭킹 페이지

--- lib/component/RestorantRankingWidget.dart
+++ lib/component/RestorantRankingWidget.dart
@@ -2,43 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:launchrabbit/MapPage.dart';
 import '../colors.dart';
 
-class RestorantRankingWidget extends StatelessWidget {
-  // 박스안에 들어가는 식당 정보들
+class RestorantRankingWidget extends StatefulWidget {
   final String name;
-  final String menu;
-  final bool isStar;
-  final int maxSeats;
-  final int seats;
-  final double km;
+  final int visitCount; // 방문횟수
+  final int rank;
   final String area;
 
-  const RestorantRankingWidget(
-      {Key? key,
-      required this.area,
-      required this.name,
-      required this.menu,
-      required this.km,
-      required this.isStar,
-      required this.maxSeats,
-      required this.seats})
-      : super(key: key);
+  const RestorantRankingWidget({
+    Key? key,
+    required this.area,
+    required this.name,
+    required this.visitCount,
+    required this.rank,
+  }) : super(key: key);
+
+  @override
+  _RestorantRankingWidgetState createState() => _RestorantRankingWidgetState();
+}
+
+class _RestorantRankingWidgetState extends State<RestorantRankingWidget> {
+  Color statusColor = primaryColor;
+  Color mainColor = primaryColor;
+  bool star = false; // Initialized with a default value
+  
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // 컬러도 오렌지색으로 미리 정의해놓음
-    Color statusColor = primaryColor;
-    Color mainColor = primaryColor;
-    bool star = isStar;
-
-    // 시트가 꽉차면 빨강이고, 꽉 안차면 초록
-    if (maxSeats - seats > 8) {
-      statusColor = safeColor;
-    } else if (maxSeats - seats > 4) {
-      statusColor = primaryColor;
-    } else {
-      statusColor = warningColor;
-    }
-
     // 텍스트 스타일을 미리 정의해놓음
     // 사이즈랑, 컬러 등...
     TextStyle statusStyle = TextStyle(
@@ -52,86 +46,14 @@ class RestorantRankingWidget extends StatelessWidget {
       fontSize: 12,
       color: textGray,
     );
-    TextStyle kmStyle =
-        TextStyle(fontSize: 12, color: mainBlack, fontWeight: FontWeight.w600);
-    return GestureDetector( // 식당 위젯 클릭되도록 감싼거
-        onTap: () {},   // 여기에 이벤트를 넣으면 식당 위젯 기능이 활성화
-        child: Container(   // 식당 위젯 하나가 컨테이너
-          padding: EdgeInsets.fromLTRB(16, 16, 16, 0),  // 빈공간
-          decoration: BoxDecoration(boxShadow: [        // 박스를 꾸민것
-            defaultShadow,          // 그림자 효과
-            // 흰색, 박스 동그라미 처리
-          ], color: mainWhite, borderRadius: BorderRadius.circular(16)),
-          child: Center(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    // 테이블 정보가 Row 하나
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.person_outline,
-                          color: statusColor,
-                        ),
-                        Text(seats.toString(), style: statusStyle),
-                        Text(
-                          '/',
-                          style: statusStyle,
-                        ),
-                        Text(maxSeats.toString(), style: statusStyle),
-                      ],
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 4)),
-                  Text(
-                    name,
-                    style: nameStyle,
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 4)),
-                  Text(
-                    menu,
-                    style: menuStyle,
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 6)),
-                  Text(
-                    km.toString() + 'km',
-                    style: kmStyle,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          //isStar조정
-                        },
-                        icon: isStar
-                            ? Icon(Icons.star, size: 20)
-                            : Icon(Icons.star_border, size: 20),
-                        color: mainColor,
-                      ),
-                      Text(
-                        '|',
-                        style: menuStyle,
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => MapPage()),
-                          );
-                        },
-                        icon: Icon(
-                          Icons.location_on,
-                          size: 20,
-                        ),
-                        color: mainColor,
-                      )
-                    ],
-                  ),
-                ]),
-          ),
-        ));
+    TextStyle kmStyle = TextStyle(
+      fontSize: 12,
+      color: mainBlack,
+      fontWeight: FontWeight.w600,
+    );
+
+    return GestureDetector(
+      
+    );
   }
 }
--- lib/visitRanking_screen.dart
+++ lib/visitRanking_screen.dart
@@ -5,10 +5,31 @@ import 'star_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:launchrabbit/component/WidgetList.dart';
 import './component/RestorantWidget.dart';
+import 'package:launchrabbit/main.dart';
+import 'enums.dart';
+import 'package:launchrabbit/colors.dart';
+import 'package:launchrabbit/component/AreaButton.dart';
+import 'package:launchrabbit/component/ExpandableFab.dart';
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
+import 'package:launchrabbit/component/RestorantWidget.dart';
+
 
 
 // 방문기록 랭킹 페이지
-class VisitRankingPage extends StatelessWidget {
+class VisitRankingPage extends StatefulWidget {
+const VisitRankingPage({super.key});
+
+  @override
+  State<VisitRankingPage> createState() => _VisitRankingState();
+}
+class _VisitRankingState extends State<VisitRankingPage>
+{
+  Area _selectedArea = Area.Sinjeongmun;
+    void updateArea(Area area) {
+      setState(() {
+        _selectedArea = area;
+      });
+  }
   List<RestorantWidget> stars = [
     RestorantWidget(
       name: '금암피순대',
@@ -19,13 +40,16 @@ class VisitRankingPage extends StatelessWidget {
       seats: 38,
       isStar: true,
     ),
+    
+  ];
+  List<RestorantWidget> test = [
     RestorantWidget(
-      name: '무진장순두부',
-      menu: '순두부찌개 - 돈까스 -청국장',
-      area: '사대부고',
-      km: 1.8,
-      maxSeats: 16,
-      seats: 8,
+      name: '리스트가 문제냐?',
+      menu: '피순대 - 순대국밥 - 선지국밥',
+      area: '',
+      km: 0.8,
+      maxSeats: 40,
+      seats: 38,
       isStar: true,
     ),
   ];
@@ -45,8 +69,71 @@ class VisitRankingPage extends StatelessWidget {
         padding: EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [WidgetList(widgetList: stars)],
+          children:<Widget>[
+            SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          AreaButton(
+                            imagePath: 'assets/images/Sinjeongmun.png',
+                            buttonText: '신정문',
+                            color: _selectedArea == Area.Sinjeongmun
+                                ? highlightColor
+                                : seconderyColor,
+                            onPressed: () {
+                              updateArea(Area.Sinjeongmun);
+                            },
+                          ),
+                          AreaButton(
+                            imagePath: 'assets/images/Gujeongmun.png',
+                            buttonText: '구정문',
+                            color: _selectedArea == Area.Gujeongmun
+                                ? highlightColor
+                                : seconderyColor,
+                            onPressed: () {
+                              updateArea(Area.Gujeongmun);
+                            },
+                          ),
+                          AreaButton(
+                            imagePath: 'assets/images/Sadaebugo.png',
+                            buttonText: '사대부고',
+                            color: _selectedArea == Area.Sadaebugo
+                                ? highlightColor
+                                : seconderyColor,
+                            onPressed: () {
+                              updateArea(Area.Sadaebugo);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    // 새로운 위젯 (완성하고 list 위젯형식으로 만들어보자..)
+                    Container(
+                      padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                      decoration: BoxDecoration(
+                        boxShadow: [defaultShadow],
+                        color: mainWhite,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                        child: Container(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image.asset('assets/images/rank_1.png',
+                                  width: 50,
+                                  height: 50,
+                                  ),
+                                  // Icon(
+                                  //   Icons.person_outline,
+                                  // ),
+                                  Text('금암순대 ', style: TextStyle(fontSize: 20)),
+                                  Text('방문 횟수: 10', style: TextStyle(fontSize: 20)),
+                                ],
+                            ),            
+                          ),
+                        ),   
+          ],   
         ),
-      ),
+      ), 
     );
   }}
